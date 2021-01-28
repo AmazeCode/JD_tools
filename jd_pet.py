e5ec77b3469768edcfc21b73d7e6ba918cc356c5
yadong.zhang <1558628010@qq.com>
2021-01-28 18:08:21 +0800

    新增助力码

--- jd_pet.py
+++ jd_pet.py
@@ -18,9 +18,9 @@ retainFoodAmountLimit = 40  # 完成10次喂养任务的基础上,希望food始�
 shareCodes = [
     "MTAxODc2NTE0NzAwMDAwMDAwNTcxOTg3NQ==", # Smile
     "MTAxODc2NTEzMTAwMDAwMDAxNzU2NzExOQ==", # Dong
+    "MTE1NDQ5MzYwMDAwMDAwNDM5NjkxMDk=", # 当小黑遇上小白
     "MTE1NDQ5MzYwMDAwMDAwNDM5Mjg4MjE=", # 李幸福
     "MTE1NDUwMTI0MDAwMDAwMDQzOTM4NDgx", # 李源儿
-
     "MTAxODc2NTEzMTAwMDAwMDAwOTYwNDkzMQ==", 
     "MTAxODcxOTI2NTAwMDAwMDAwMTY0NTc4OQ==", 
     "MTAxODc2NTEzMDAwMDAwMDAyNjYzMDQ3MQ=="
